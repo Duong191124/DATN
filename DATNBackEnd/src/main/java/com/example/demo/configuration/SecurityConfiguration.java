6752270da68e30a921fdcb7971efc6f164ff97a3
Duong191124 <119329339+Duong191124@users.noreply.github.com>
2024-08-30 16:21:14 +0700

    .

--- DATNBackEnd/src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ DATNBackEnd/src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -1,86 +1,86 @@
-//package com.example.demo.configuration;
-//import com.example.demo.utility.CustomJwtGrantedAuthoritiesConverter;
-//import com.nimbusds.jose.jwk.source.ImmutableSecret;
-//import com.nimbusds.jose.util.Base64;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.convert.converter.Converter;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-//import org.springframework.security.oauth2.jwt.Jwt;
-//import org.springframework.security.oauth2.jwt.JwtDecoder;
-//import org.springframework.security.oauth2.jwt.JwtEncoder;
-//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-//import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
-//import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-//import org.springframework.security.web.SecurityFilterChain;
-//import javax.crypto.SecretKey;
-//import javax.crypto.spec.SecretKeySpec;
-//import java.util.Collection;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.stream.Collectors;
-//
-//
-//
-//@Configuration
-//@EnableMethodSecurity
-//public class SecurityConfiguration {
-//
-//    @Value("${secret.key}")
-//    private String secretKey;
-//
-//    public final MacAlgorithm macAlgorithm = MacAlgorithm.HS512;
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(authz -> authz
-//                        .requestMatchers("/api/v1/role/**").permitAll()
-//                        .anyRequest().permitAll())
-//                .oauth2ResourceServer(oauth2 -> oauth2
-//                        .jwt(jwt -> jwt.jwtAuthenticationConverter(this.jwtAuthenticationConverter()))
-//                )
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .formLogin(f -> f.disable())
-//                .csrf(c -> c.disable());
-//
-//        return http.build();
-//    }
-//
-//    @Bean
-//    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-//        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new CustomJwtGrantedAuthoritiesConverter());
-//        return jwtAuthenticationConverter;
-//    }
-//
-//    private SecretKey getSecretKey() {
-//        byte[] keyBytes = Base64.from(secretKey).decode();
-//        return new SecretKeySpec(keyBytes, 0, keyBytes.length, macAlgorithm.getName());
-//    }
-//
-//    @Bean
-//    public JwtEncoder jwtEncoder() {
-//        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
-//    }
-//
-//    @Bean
-//    public JwtDecoder jwtDecoder() {
-//        return NimbusJwtDecoder.withSecretKey(getSecretKey()).macAlgorithm(macAlgorithm).build();
-//    }
-//
-//
-//}
-//
+package com.example.demo.configuration;
+import com.example.demo.utility.CustomJwtGrantedAuthoritiesConverter;
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.util.Base64;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+
+    @Value("${secret.key}")
+    private String secretKey;
+
+    public final MacAlgorithm macAlgorithm = MacAlgorithm.HS512;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/v1/role/**").permitAll()
+                        .anyRequest().permitAll())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(this.jwtAuthenticationConverter()))
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(f -> f.disable())
+                .csrf(c -> c.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new CustomJwtGrantedAuthoritiesConverter());
+        return jwtAuthenticationConverter;
+    }
+
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.from(secretKey).decode();
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, macAlgorithm.getName());
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withSecretKey(getSecretKey()).macAlgorithm(macAlgorithm).build();
+    }
+
+
+}
+
--- DATNBackEnd/src/main/java/com/example/demo/utility/CustomJwtGrantedAuthoritiesConverter.java
+++ DATNBackEnd/src/main/java/com/example/demo/utility/CustomJwtGrantedAuthoritiesConverter.java
@@ -1,26 +1,26 @@
-//package com.example.demo.utility;
-//
-//import org.springframework.core.convert.converter.Converter;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//import org.springframework.security.oauth2.jwt.Jwt;
-//
-//import java.util.Collection;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.stream.Collectors;
-//
-//public class CustomJwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
-//
-//    @Override
-//    public Collection<GrantedAuthority> convert(Jwt jwt) {
-//        // Lấy các quyền từ claim claim.authorities
-//        Map<String, Object> claims = jwt.getClaims();
-//        Map<String, Object> claim = (Map<String, Object>) claims.get("claim");
-//        List<Map<String, String>> authorities = (List<Map<String, String>>) claim.get("authorities");
-//
-//        return authorities.stream()
-//                .map(authority -> new SimpleGrantedAuthority(authority.get("role")))
-//                .collect(Collectors.toList());
-//    }
-//}
+package com.example.demo.utility;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CustomJwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        // Lấy các quyền từ claim claim.authorities
+        Map<String, Object> claims = jwt.getClaims();
+        Map<String, Object> claim = (Map<String, Object>) claims.get("claim");
+        List<Map<String, String>> authorities = (List<Map<String, String>>) claim.get("authorities");
+
+        return authorities.stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.get("role")))
+                .collect(Collectors.toList());
+    }
+}
