5c3cfe2d246078ca947f23eda95077f0bc88167e
Jengdoo <macdinhduy28052004@gmail.com>
2024-10-28 19:39:10 +0700

    update again

--- DATNFrontEnd/src/component/layout/admin/counter.sale/counter.slale.customer.jsx
+++ DATNFrontEnd/src/component/layout/admin/counter.sale/counter.slale.customer.jsx
@@ -10,7 +10,7 @@ const CounterSaleCustomer = ({
   setPage,
 }) => {
   const [modalVisible, setModalVisible] = useState(false);
-  const [selectedCustomer, setSelectedCustomer] = useState(null); // Nhân viên đã chọn
+  const [selectedCustomer, setSelectedCustomer] = useState(null);
 
   const columns = [
     {
@@ -29,7 +29,7 @@ const CounterSaleCustomer = ({
           onClick={() => handleCustomerSelect(record)}
           disabled={
             selectedCustomer !== null && selectedCustomer.id !== record.id
-          } // Chỉ cho phép chọn 1 nhân viên
+          }
         >
           {selectedCustomer?.id === record.id ? "Đã chọn" : "Chọn"}
         </Button>
@@ -42,7 +42,7 @@ const CounterSaleCustomer = ({
           type="default"
           danger
           onClick={() => handleCustomerDeselect(record)}
-          disabled={selectedCustomer?.id !== record.id} // Chỉ hiển thị "Hủy chọn" khi nhân viên này đã chọn
+          disabled={selectedCustomer?.id !== record.id}
         >
           Hủy chọn
         </Button>
@@ -65,8 +65,8 @@ const CounterSaleCustomer = ({
   // Hàm xử lý hủy chọn customer
   const handleCustomerDeselect = (customer) => {
     if (selectedCustomer?.id === customer.id) {
-      setSelectedCustomer(null); // Hủy chọn nhân viên hiện tại
-      onCustomerSelect(null); // Thông báo cho component cha rằng không có nhân viên nào được chọn
+      setSelectedCustomer(null);
+      onCustomerSelect(null);
       notification.info({
         message: "Đã hủy chọn khách hàng",
         description: `Khách hàng ${customer.name} đã bị hủy chọn.`,
